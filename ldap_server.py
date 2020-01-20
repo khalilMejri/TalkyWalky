522b0b0bfecbb314d15e459012699199cd7b0178
Khalil Mejri <khalil.mejri.39566@gmail.com>
2020-01-20 01:03:29 +0100

    added step - login with LDAP

---
+++ ldap_server.py
@@ -0,0 +1,74 @@
+import ldap
+
+
+class LdapService():
+
+    ldap_server = "ldap://192.168.56.3:389"  # host address
+    ldap_ou = "security"  # organization unit
+    ldap_group = "security-group"  # organization sub-group
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def login(self):
+
+        # the following is the user_dn format provided by the ldap server
+
+        # admin domain
+        # user_dn = "cn="+self.username+",dc=insat,dc=tn"
+
+        # organization user domain
+        user_dn = "cn=" + self.username + ",cn=" + self.ldap_group + ",ou=" + \
+            self.ldap_ou + ",dc=insat,dc=tn"
+
+        print(user_dn)
+
+        # base domain
+        base_dn = "cn=" + self.ldap_group + ",ou=" + self.ldap_ou + ",dc=insat,dc=tn"
+
+        # start connection
+        ldap_client = ldap.initialize(self.ldap_server)
+        # search for specific user
+        search_filter = "cn=" + self.username
+
+        try:
+            # if authentication successful, get the full user data
+            ldap_client.bind_s(user_dn, self.password)
+            result = ldap_client.search_s(
+                base_dn, ldap.SCOPE_SUBTREE, search_filter)
+
+            # return all user data results
+            ldap_client.unbind_s()
+            print(result)
+            return None
+        except ldap.INVALID_CREDENTIALS:
+            ldap_client.unbind()
+            print("Wrong username or password..")
+            return "Wrong username or password.."
+        except ldap.SERVER_DOWN:
+            print("Server is down at the moment, please try again later!")
+            return "Server is down at the moment, please try again later!"
+        except ldap.LDAPError:
+            ldap_client.unbind_s()
+            print("Authentication error!")
+            return "Authentication error!"
+
+    def register(self):
+
+        # the following is the user_dn format provided by the ldap server
+
+        # admin domain
+        user_dn = "cn="+self.username+",dc=insat,dc=tn"
+
+
+# TESTING CONNECTION, also IGNORE the ERRORS since there's NO ERROR,
+# dunno why they are being thrown,
+# the attributes DO EXIST in ldap module and I VERIFIED it!
+# CONNECTION WILL ONLY WORK WHEN MY SERVER IS UP
+
+
+# # test case
+# s = LdapService(username="guest", password="0000")
+# s.login()
+# s.register()
--- login.py
+++ login.py
@@ -1,9 +1,56 @@
-from tkinter import Label, Entry, Button, Tk, Radiobutton, IntVar
+from tkinter import Label, Entry, Button, Tk, Radiobutton, IntVar, StringVar, Toplevel, Canvas
+from ldap_server import LdapService
+import time
+
+
+def Login(event=None):
+    if USERNAME.get() == "" or PASSWORD.get() == "":
+        error_label.config(
+            text="Please complete the required field!", fg="#0F0F0F", bg="#33FF33")
+    else:
+        ldap_s = LdapService(username=USERNAME.get(), password=PASSWORD.get())
+        result = ldap_s.login()
+        if not result:
+            # HomeWindow()
+            USERNAME.set("")
+            PASSWORD.set("")
+            error_label.config(text="Sucess", fg="#33FF33", bg="#336633")
+
+        else:
+            error_label.config(text=result, fg="#0F0F0F", bg="#33FF33")
+
+
+def HomeWindow():
+    global Home
+    root.withdraw()
+    Home = Toplevel()
+    Home.title("Python: Simple Login Application")
+    width = 600
+    height = 500
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    x = (screen_width/2) - (width/2)
+    y = (screen_height/2) - (height/2)
+    root.resizable(0, 0)
+    Home.geometry("%dx%d+%d+%d" % (width, height, x, y))
+    lbl_home = Label(Home, text="Successfully Login!",
+                     font=('times new roman', 20)).pack()
+    btn_back = Button(Home, text='Back', command=Back).pack(pady=20, fill=X)
+
+
+def Back():
+    Home.destroy()
+    root.deiconify()
+
 
 # main frame
 root = Tk()
 root.geometry('500x300')
 root.title("Login Form")
+
+# data binding
+USERNAME = StringVar(root)
+PASSWORD = StringVar(root)
 
 # Login form
 label_0 = Label(root, text="LOGIN", width=20, font=("bold", 20))
@@ -14,22 +61,27 @@ sub_label = Label(root, text="Discuss your favorite technology with the communit
                   width=45, font=("bold", 12))
 sub_label.place(x=45, y=65)
 
-# Email label & entry
-label_1 = Label(root, text="Email", width=20, font=("bold", 10))
+# Username label & entry
+label_1 = Label(root, text="Username *", width=20, font=("bold", 10))
 label_1.place(x=80, y=130)
-entry_1 = Entry(root)
+entry_1 = Entry(root, textvariable=USERNAME)
 entry_1.place(x=240, y=130)
 
 # Password label & entry
-label_2 = Label(root, text="Password", width=20, font=("bold", 10))
+label_2 = Label(root, text="Password *", width=20, font=("bold", 10))
 label_2.place(x=68, y=180)
-entry_2 = Entry(root)
+entry_2 = Entry(root, textvariable=PASSWORD, show="*")
 entry_2.place(x=240, y=180)
 
 # Submit button
 btn = Button(root, text='Connect', width=20, bg='brown',
-             fg='white')
+             fg='white', command=Login)
 btn.place(x=180, y=250)
+btn.bind('<Return>', Login)
+
+# Error label
+error_label = Label(root, width=60, font=("bold", 8))
+error_label.place(x=65, y=220)
 
 # theme color hacker
 root.config(bg="#0F0F0F")
@@ -41,7 +93,10 @@ entry_1.config(bg="#0F0F0F", fg="#33FF33", insertbackground="#33FF33")
 entry_2.config(bg="#0F0F0F", fg="#33FF33", insertbackground="#33FF33")
 btn.config(bg="#0F0F0F", fg="#FFFFFF",
            activebackground="#0F0F0F", activeforeground="#FFFFFF")
+error_label.config(bg="#0F0F0F")
+
 
 # it is use for display the registration form on the window
+root.resizable(200, 120)
 root.mainloop()
-print("registration form  seccussfully created...")
+print("login form seccussfully created...")
