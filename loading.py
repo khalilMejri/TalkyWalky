132aad050901536be514d1563fb386bc2854e2f8
Khalil Mejri <khalil.mejri.39566@gmail.com>
2020-01-23 00:43:05 +0100

    e2e authentication & registration

---
+++ loading.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageTk
+import tkinter as tk
+import time
+
+IMAGE_PATH = './imgs/5.png'
+WIDTH, HEIGTH = 600, 400
+
+screen = tk.Toplevel()
+screen.geometry('{}x{}'.format(WIDTH, HEIGTH))
+
+canvas = tk.Canvas(screen, width=WIDTH, height=HEIGTH)
+canvas.pack()
+
+img = ImageTk.PhotoImage(Image.open(IMAGE_PATH).resize(
+    (WIDTH, HEIGTH), Image.ANTIALIAS))
+# Keep a reference in case this code is put in a function.
+canvas.background = img
+bg = canvas.create_image(0, 0, anchor=tk.NW, image=img)
+
+# Put a tkinter widget on the canvas.
+# button = tk.Button(screen, text="Start")
+# button_window = canvas.create_window(10, 10, anchor=tk.NW, window=button)
+
+screen.after(2000, screen.destroy)
+screen.mainloop()
+if True:
+    import main
